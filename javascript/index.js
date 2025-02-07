a718a18c584564747455bbe86fc5d2c322d442b2
titansfandrew <drewdulaney5@gmail.com>
2025-02-06 18:55:25 -0600

    partone

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+document.getElementById("github").onclick = function () {
+    window.open("https://github.com/titansfandrew", "_blank", "noopener, noreferrer")
+};
+
+document.getElementById("linkedin").onclick = function () {
+    window.open("https://www.linkedin.com/in/drew-dulaney-6728a8346/", "_blank", "noopener, noreferrer")
+};
