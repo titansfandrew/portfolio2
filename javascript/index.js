a1ffba5e497e88466b9f3d3254502650f19255cb
titansfandrew <drewdulaney5@gmail.com>
2025-02-08 10:08:35 -0600

    box2

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("github").onclick = function () {
-    window.open("https://github.com/titansfandrew", "_blank", "noopener, noreferrer")
-};
-
-document.getElementById("linkedin").onclick = function () {
-    window.open("https://www.linkedin.com/in/drew-dulaney-6728a8346/", "_blank", "noopener, noreferrer")
-};
